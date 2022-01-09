763d2da0082686ad17485a5fdc1dbc75a133b79c
Denisa Pap <papdeni11@gmail.com>
2022-01-09 03:34:07 +0200

    create layout for adding a new task

--- Android project/app/src/main/java/com/upt/cti/sma_android_project/HomeActivity.java
+++ Android project/app/src/main/java/com/upt/cti/sma_android_project/HomeActivity.java
@@ -1,33 +1,69 @@
 package com.upt.cti.sma_android_project;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private Toolbar toolbarHomeScreen;
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        toolbarHomeScreen =  findViewById(R.id.home_toolbar);
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        toolbarHomeScreen =  findViewById(R.id.home_toolbar);
         setSupportActionBar(toolbarHomeScreen);
         getSupportActionBar().setTitle("Home Screen");
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        recyclerView =  findViewById(R.id.recyclerView);
+        floatingActionButton =  findViewById(R.id.fab);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTask();
+
+            }
+        });
+    }
+
+    private void addTask() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.input_file, null);
+        dialog.setView(view);
+
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.setCancelable(false);
+        alertDialog.show();
     }
 
     @Override
